2a62f5f1f3dd48bb4ce3c3d4839c3a1f9859ba75
IanCist <iantcisternas@gmail.com>
2023-12-21 13:47:33 -0500

    added navbar and added onto my madlib

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let adjective = prompt('Enter an adjective:')
-let noun = prompt('Enter a noun:')
-let verb = prompt('Enter a verb:')
-let adverb = prompt('Enter an adverb:')
+let adjective = prompt('Enter an adjective:');
+let noun = prompt('Enter a noun:');
+let verb = prompt('Enter a verb:');
+let adverb = prompt('Enter an adverb:');
+let animal = prompt('Enter an animal:');
+let place = prompt('Enter a place:');
 
-let story = `once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}. 
+They were joined by a ${adjective} ${animal} who loved to ${verb} in ${place}.`;
 
-let madLibOutputDiv = document.getElementById(`madLibOutput`)
+let madLibOutputDiv = document.getElementById('madLibOutput');
 
-madLibOutputDiv.innerHTML = `<p> ${story} </P>`
+madLibOutputDiv.innerHTML = `<p>${story}</p>`;
