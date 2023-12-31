b0e21a8a4c3ca80a01eb176b252be7a882f38910
IanCist <144719417+IanCist@users.noreply.github.com>
2023-12-31 16:17:54 -0300

    changed an input in js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let adjective = prompt('Enter an adjective:');
+let place = prompt('Enter a place:');
 let noun = prompt('Enter a noun:');
 let verb = prompt('Enter a verb:');
 let adverb = prompt('Enter an adverb:');
@@ -8,7 +8,7 @@ let famousPerson = prompt('Enter the name of a famous person:');
 let number = prompt('Enter a number:');
 let emotion = prompt('Enter an emotion:');
 
-let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}. 
+let story = `Once upon a time, in ${place} ${noun}, a ${verb} ${noun} ${adverb} ${verb}. 
     They were joined by a ${adjective} ${animal} who loved to ${verb} in ${place}. 
     The group encountered ${number} ${adjective} ${noun}s led by the famous ${famousPerson}. 
     The atmosphere was filled with ${emotion} as they all ${verb} together in ${place}.`;
